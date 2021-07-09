00b12c6c1346c647dc4ff0066ce68d19f079cc33
iammitpatel <86827679+iammitpatel@users.noreply.github.com>
2021-07-09 15:25:30 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(UdemyStarter());
+}
+
+class UdemyStarter extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text("hello"),
+    );
+  }
+}
